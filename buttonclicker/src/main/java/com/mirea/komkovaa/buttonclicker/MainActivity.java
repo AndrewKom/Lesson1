a420e9fa49a9aed7f00dde9c52263f297b6d4221
AndrewKom <sinhron2020@yandex.ru>
2025-02-26 16:33:36 +0300

    button -> text

--- buttonclicker/src/main/java/com/mirea/komkovaa/buttonclicker/MainActivity.java
+++ buttonclicker/src/main/java/com/mirea/komkovaa/buttonclicker/MainActivity.java
@@ -1,9 +1,10 @@
-package com.mirea.komkovaa.laouttype;
+package com.mirea.komkovaa.buttonclicker;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,21 +12,35 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
 public class MainActivity extends AppCompatActivity {
+    private TextView textViewStudent;
+    private Button btnWhoAmI;
+    private Button btnItIsNotMe ;
+
+    public void onMyButtonClick(View view) {
+        textViewStudent.setText("Это не я сделал");
+        Toast.makeText(this, "Ещё один способ!", Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        TextView myTextView = (TextView) findViewById(R.id.textView2);
-        myTextView.setText("New text in MIREA");
+        textViewStudent = findViewById(R.id.textView);
+        btnWhoAmI = findViewById(R.id.button);
+        btnItIsNotMe = findViewById(R.id.button2);
 
-        Button button = findViewById(R.id.button4);
-        //button.setText("MireaButton");
+        View.OnClickListener oclBtnWhoAmI = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textViewStudent.setText("Мой номер по списку № 14");
+            }
+        };
 
-        CheckBox checkBox = findViewById(R.id.checkBox2);
-        checkBox.setChecked(true);
+        btnWhoAmI.setOnClickListener(oclBtnWhoAmI);
+
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -33,8 +48,5 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-
-
     }
 }
